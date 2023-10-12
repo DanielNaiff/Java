d3bafccabe067d91c7d8e5843803adc696310075
danielnaiff <daniel.naiff@yahoo.com.br>
2023-10-12 18:27:44 -0300

    aula 2

---
+++ Aula 02/src/aula/pkg02/Caneta.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula.pkg02;
+
+/**
+ *
+ * @author danie
+ */
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    
+    void rabiscar(){
+        
+    }
+    
+    void
+    
+}

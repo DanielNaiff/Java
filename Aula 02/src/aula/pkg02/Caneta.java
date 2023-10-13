26d7882296d9e8495e9b40a92ab608ea8c03764e
danielnaiff <daniel.naiff@yahoo.com.br>
2023-10-12 23:35:19 -0300

    poo

--- Aula 02/src/aula/pkg02/Caneta.java
+++ Aula 02/src/aula/pkg02/Caneta.java
@@ -4,10 +4,6 @@
  */
 package aula.pkg02;
 
-/**
- *
- * @author danie
- */
 public class Caneta {
     String modelo;
     String cor;
@@ -15,10 +11,28 @@ public class Caneta {
     int carga;
     boolean tampada;
     
-    void rabiscar(){
-        
+    void status(){
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("está tampada? " + this.tampada);
+        System.out.println("Ponta: "+ this.ponta);
+        System.out.println("Carga: "+ this.carga);
+        System.out.println("Modelo:" + this.modelo);
     }
     
-    void
+    void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+    
+    void tampar(){
+        this.tampada = true;
+    }
+    
+    void destampar(){
+        this.tampada = false;
+    }
     
 }

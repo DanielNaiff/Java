11ef3e1c5eb66c94a82d28b69ae9060079187eac
danielnaiff <daniel.naiff@yahoo.com.br>
2023-10-13 23:30:04 -0300

    aula 3

---
+++ POO/Aula_03/src/aula/pkg02/Caneta.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula.pkg02;
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    
+    public void status(){
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("está tampada? " + this.tampada);
+        System.out.println("Ponta: "+ this.ponta);
+        System.out.println("Carga: "+ this.carga);
+        System.out.println("Modelo:" + this.modelo);
+    }
+    
+    public void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando ");
+        }
+    }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar(){
+        this.tampada = false;
+    }
+    
+}

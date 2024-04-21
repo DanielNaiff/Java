003d7ae00301288e2f763773395a0364f5bf8963
DanielNaiff <danielnaiff344@gmail.com>
2024-04-21 18:59:29 -0300

    comentários

--- Pure Java/maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,17 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
+    /**
+     * Isso é um comentário
+     *
+     */
+    //Isso é um comentário de uma linha
+    /*
+    Comentário
+    de
+    várias
+    linhas
+    */
     public static void main(String[] args){
         System.out.println("Hello World!");
     }

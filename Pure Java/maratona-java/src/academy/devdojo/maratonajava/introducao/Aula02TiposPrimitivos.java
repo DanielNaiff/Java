fe2fffae9694244dc89fda6405dce4281d0dcdce
DanielNaiff <danielnaiff344@gmail.com>
2024-04-21 23:48:17 -0300

    casting

--- Pure Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,10 +3,10 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args){
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        double salarioFloat = 2500;
+        int idade = (int) 1000000000l;
+        long numeroGrande =(long) 155.23;
+        double salarioDouble = 2000D;
+        float salarioFloat =(float) 2500.0D;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean verdadeiro = true;

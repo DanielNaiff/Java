ac3ce28723b8f0431850ab58fc89225bc13bb010
DanielNaiff <danielnaiff344@gmail.com>
2024-04-22 22:25:39 -0300

    Lógicos AND

--- Pure Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -17,5 +17,13 @@ public class Aula04Operadores {
 
         boolean isDezMaiorQueVinte = 10 > 20;
         System.out.println("Dez é maior que vinte? "+isDezMaiorQueVinte);
+
+        //&&(and) ||(or) !
+        int idade = 35;
+        float salario = 3500;
+        boolean isDentroDaLei = idade > 30 && salario > 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLei "+ isDentroDaLei);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+ isDentroDaLeiMenorQueTrinta);
     }
 }

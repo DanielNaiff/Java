2b6653f0946d7b7767b96fa30830a4b837598938
DanielNaiff <danielnaiff344@gmail.com>
2024-05-25 16:56:10 -0300

    get, set and construct

--- POO/GetSetConstruc/src/Main.java
+++ POO/GetSetConstruc/src/Main.java
@@ -2,13 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-
-        c1.cor = "Azul";
-        c1.ponta = 0.5f;
-        c1.tampada = false;
-        c1.tampar();
+        Caneta c1 = new Caneta("NIC", "Amarela", 0.4f);
         c1.status();
-        c1.rabiscar();
     }
 }
--- POO/Visibility/src/Caneta.java
+++ POO/Visibility/src/Caneta.java
@@ -1,11 +1,11 @@
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
 
-    void status(){
+    public void status(){
         System.out.println("Carga: "+this.carga);
         System.out.println("Uma caneta "+ this.cor);
         System.out.println("Modelo: "+this.modelo);
@@ -13,7 +13,7 @@ public class Caneta {
         System.out.println("Esta tampada "+ this.tampada);
     }
 
-    void rabiscar(){
+    public void rabiscar(){
         if (this.tampada){
             System.out.println("Erro! Estou tampada!");
         }else{
@@ -22,11 +22,11 @@ public class Caneta {
 
     }
 
-    void tampar(){
+    public void tampar(){
         this.tampada = true;
     }
 
-    void destampar(){
+    public void destampar(){
         this.tampada = false;
     }
 }

c2f6d373abbe3e4cb783f2ce333d0ce07b36a1da
DanielNaiff <danielnaiff344@gmail.com>
2024-07-17 16:47:45 -0300

    Introduction POO

---
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.domain.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+    }
+}

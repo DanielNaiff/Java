3fee29688b45971bf5ec1df707d777190d0b1cf7
DanielNaiff <danielnaiff344@gmail.com>
2024-07-19 15:15:55 -0300

    exercicio associacao

--- Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/domain/Professor.java
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Gassociacao/domain/Professor.java
@@ -2,9 +2,38 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.domain;
 
 public class Professor {
     private String nome;
+    private String especialidade;
+    private Seminario[] semeinarios;
+
+    public Professor(String nome, String especialidade) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
+
+    public Professor(String nome, String especialidade, Seminario[] semeinarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.semeinarios = semeinarios;
+    }
 
     public Professor(String nome) {
         this.nome = nome;
+    }
+
+    public Seminario[] getSemeinarios() {
+        return semeinarios;
+    }
+
+    public void setSemeinarios(Seminario[] semeinarios) {
+        this.semeinarios = semeinarios;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
     }
 
     public String getNome() {

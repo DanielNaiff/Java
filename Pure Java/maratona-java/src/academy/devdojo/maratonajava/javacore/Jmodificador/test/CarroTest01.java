d34c1d564f7aaac7bc2a3bbbb8803ef3950bc47b
DanielNaiff <danielnaiff344@gmail.com>
2024-07-19 16:30:58 -0300

    tipo de referencia

---
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificador/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Jmodificador.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificador.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}

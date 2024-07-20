3c27200f6bdbd468c4ed9f4aaa92e47a19883c70
DanielNaiff <danielnaiff344@gmail.com>
2024-07-19 21:32:24 -0300

    Sobrescrita de métodos

--- Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -2,13 +2,16 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.domain.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("tsabasa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("akira", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("tsabasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }

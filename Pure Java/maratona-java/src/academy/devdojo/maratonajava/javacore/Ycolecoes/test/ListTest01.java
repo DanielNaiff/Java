7533c3d819c6c3e05f2420998634e0d829eac5f4
DanielNaiff <danielnaiff344@gmail.com>
2024-07-21 11:28:12 -0300

    colletction part 7 list

--- Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -5,12 +5,24 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList<>();
-        nomes.add("Willain");
+        List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
+        nomes.add("Willian");
         nomes.add("devdojo Academy");
-
+        nomes2.add("Suane");
+        nomes2.add("Academy");
+        nomes.addAll(nomes2);
+        System.out.println(nomes);
         for(String nome: nomes){
             System.out.println(nome);
+//            nomes.add("Suane");
         }
+
+        int size = nomes.size();
+
+        System.out.println("---------------");
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }

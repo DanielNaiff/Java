bc2d1a51040f13ac5cf10cc6f36fc3921140d34c
DanielNaiff <danielnaiff344@gmail.com>
2024-07-21 12:06:38 -0300

    collection part 8 list

---
+++ Pure Java/maratona-java/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneListTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Smartphone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartphoneListTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("22222", "Pixel");
+        Smartphone s3 = new Smartphone("33333", "Samsung");
+        List<Smartphone> smartphones = new ArrayList<>(6);
+        smartphones.add(s1);
+        smartphones.add(s2);
+        smartphones.add(s3);
+        for(Smartphone smartphone: smartphones){
+            System.out.println(smartphone);
+        }
+        Smartphone s4 = new Smartphone("22222", "Pixel");
+        smartphones.add(0,s4);
+        System.out.println(smartphones.contains(s4));
+        int indexSmartphone = smartphones.indexOf(s4);
+        System.out.println(indexSmartphone);
+        System.out.println(smartphones.get(3));
+    }
+}

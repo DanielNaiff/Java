d829ea5bd85a33ce25ffa4477f6c92b1981e2e4e
Daniel Naiff <danielnaiff344@gmail.com>
2024-12-22 12:27:35 -0300

    solucao ruim

---
+++ COMPLETE Java Object Oriented Programming +Projects/Section 14 exception/Solucao/src/Reservation.java
@@ -0,0 +1,62 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(){}
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkin;
+        this.checkOut = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public String updateDates(Date checkin, Date checkout){
+
+        Date now = new Date();
+        if(checkIn.before(now) || checkOut.before(now)){
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if(!checkOut.after(checkIn)){
+            return "Error in reservation: Check-out date must be after checkin date";
+        }
+
+        this.checkIn = checkin;
+        this.checkOut = checkout;
+
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return "Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: "+ sdf.format(checkOut) + ", " + duration() + " nights";
+    }
+
+}

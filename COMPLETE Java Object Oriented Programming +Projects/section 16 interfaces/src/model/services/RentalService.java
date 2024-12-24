a224fddb4ebcd5bf2f2dac685b4150df68b4df79
Daniel Naiff <danielnaiff344@gmail.com>
2024-12-24 15:56:11 -0300

    Section 16 solucao sem interface

---
+++ COMPLETE Java Object Oriented Programming +Projects/section 16 interfaces/src/model/services/RentalService.java
@@ -0,0 +1,37 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+    private Double pricePerDay;
+    private  Double getPricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double getPricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.getPricePerHour = getPricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental){
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes/60;
+
+        double basicPayment;
+
+        if(hours <= 12.0){
+            basicPayment = pricePerDay * Math.ceil(hours);
+        }else{
+            basicPayment = pricePerDay * Math.ceil(hours/24);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
